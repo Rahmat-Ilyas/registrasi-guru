d172d217efb53eac03a67a781c025fadaf363d74
rahmat-ilyas <rahmat.ilyas142@gmail.com>
2022-01-21 21:58:58 +0800

    Template

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -6,5 +6,24 @@ use Illuminate\Http\Request;
 
 class AdminController extends Controller
 {
-    //
+    // public function __construct()
+    // {
+    //     $this->middleware('auth:admin');
+    //     $this->cancelAntrian();
+    // }
+
+    public function home()
+    {
+        return view('admin/home');
+    }
+
+    public function page($page)
+    {
+        return view('admin/' . $page);
+    }
+
+    public function pagedir($dir = NULL, $page)
+    {
+        return view('admin/' . $dir . '/' . $page);
+    }
 }
